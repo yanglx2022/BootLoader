34f837142315f2a8c5e9caa3421b4968867817b7
yanglx2022 <yanglx2022@hotmail.com>
2021-07-11 17:23:20 +0800

    整理FLASH分配定义

--- User/flash.h
+++ User/flash.h
@@ -1,5 +1,11 @@
-#ifndef __flash_H
-#define __flash_H
+/**********************
+* 读写FLASH
+**********************/
+#ifndef __FLASH_H
+#define __FLASH_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 #include "stm32f10x.h"
 
@@ -12,9 +18,20 @@
 	#define FLASH_SECTOR_SIZE		2048
 #endif	
 
-// 用户区设置(非代码区域)
-#define USER_SECTOR_COUNT			35
-#define USER_FLASH_BASE				(0x08000000 + (FLASH_SIZE - USER_SECTOR_COUNT) * 1024)		// 起始地址
+// FLASH分配(共64K)
+#define	FLASH_BOOT_SIZE				(1024*17)
+#define FLASH_APP_SIZE				(1024*12)
+#define FLASH_FS_SIZE					(1024*23)
+#define FLASH_UDISK_SIZE			FLASH_APP_SIZE
+
+#define	FLASH_BOOT_BASE				(0x08000000)
+#define	FLASH_APP_BASE				(FLASH_BOOT_BASE + FLASH_BOOT_SIZE)
+#define	FLASH_FS_BASE					(FLASH_APP_BASE + FLASH_APP_SIZE)
+#define	FLASH_UDISK_BASE			(FLASH_FS_BASE + FLASH_FS_SIZE)
+
+// 非代码区域
+#define USER_SECTOR_COUNT			((FLASH_FS_SIZE + FLASH_UDISK_SIZE) / FLASH_SECTOR_SIZE)
+#define USER_FLASH_BASE				FLASH_FS_BASE
 
 
 // 从指定地址开始读数据
@@ -23,5 +40,9 @@ void Flash_Read(uint32_t address, uint8_t* buffer, uint32_t length);
 void Flash_Write(uint32_t address, const uint8_t* buffer, uint32_t length);
 
 
+#ifdef __cplusplus
+}
 #endif
+#endif /*__FLASH_H */
+
 
